d2e2f6c30fbc25435c721fb4294574dcadc933f0
Tommy Parnell <tommy@terribledev.io>
2021-03-27 17:08:15 -0400

    stop

--- server/app/worker/dist/routes/index.js
+++ server/app/worker/dist/routes/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const Users_1 = __importDefault(require("./Users"));
+const Worker_1 = __importDefault(require("./Worker"));
 // Init router and path
 const router = express_1.Router();
 // Add sub-routes
 router.use('/users', Users_1.default);
+router.use('/worker', Worker_1.default);
 // Export the base-router
 exports.default = router;
